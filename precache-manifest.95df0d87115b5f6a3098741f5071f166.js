2485f9ae66ca92be261d95635eea30b7f7fe6218
Rafael V. dos Santos <rvsantos00@gmail.com>
2020-01-25 22:15:45 -0300

    Updates

---
+++ precache-manifest.95df0d87115b5f6a3098741f5071f166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a1ddc1df0b135a1efdbb466e657184",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9aced02c71052246f674",
+    "url": "/monster-rolodex/static/css/main.2b259d6c.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9aced02c71052246f674",
+    "url": "/monster-rolodex/static/js/main.3f107b38.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
